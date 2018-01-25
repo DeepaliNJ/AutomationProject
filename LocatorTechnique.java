7693344893a45d8016cd01c7b707b302083041fc
DeepaliNJ <35280400+DeepaliNJ@users.noreply.github.com>
2018-01-25 11:11:59 -0800

    Update LocatorTechnique.java

--- LocatorTechnique.java
+++ LocatorTechnique.java
@@ -8,15 +8,14 @@ public class LocatorTechnique {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.gecko.driver", "C://Users/dyj/Downloads/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.expedia.com/");
-	
-		driver.findElement(By.id("tab-flight-tab-hp")).click();
 		
+		driver.get("https://www.expedia.com/");	
+		driver.findElement(By.id("tab-flight-tab-hp")).click();
 		driver.findElement(By.id("flight-origin-hp-flight")).sendKeys("Seattle");
-		//driver.findElement(By.name("pass")).sendKeys("Aurangabad@10");
-		//driver.findElement(")
+		
 	}
 
 }
