a391b81bcd19e89c5a13f84dbec838b110314129
Deepali Neb <35280400+DeepaliNJ@users.noreply.github.com>
2018-02-06 15:47:03 -0800

    Update StaticElement.java
    This test case is trying to automate various dropdowns, check boxes and radio button from spicejet.com websites.

--- StaticElement.java
+++ StaticElement.java
@@ -12,11 +12,19 @@ public class StaticElement {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dyj\\Downloads\\chromedriver.exe");	
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com/");
+		//By using select() class handling static dropbox
 		Select s = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
 		s.selectByValue("2");
 		s.selectByIndex(6);
 		s.selectByVisibleText("4");
-		
+		//Handling dynamic dropbox
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='GOI']")).click();
+		driver.findElement(By.xpath("(//a[@value='DEL'])[2]")).click();
+		//Handling check box 
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_Unmr")).isSelected());
+		driver.findElement(By.id("ctl00_mainContent_chk_Unmr")).click();
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_Unmr")).isSelected());
 		
 		
 		
